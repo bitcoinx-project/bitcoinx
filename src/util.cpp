3354a88d9781aede6a4d8024760c40cce8d8bb09
bayes <bayes@gmail.com>
2018-01-16 02:19:50 +0800

    fix: dyld missing when make deploy on osx and update version

--- src/util.cpp
+++ src/util.cpp
@@ -88,7 +88,7 @@
 const int64_t nStartupTime = GetTime();
 
 const char * const BITCOIN_CONF_FILENAME = "bitcoinx.conf";
-const char * const BITCOIN_PID_FILENAME = "bitcoindx.pid";
+const char * const BITCOIN_PID_FILENAME = "bitcoinxd.pid";
 
 ArgsManager gArgs;
 bool fPrintToConsole = false;
